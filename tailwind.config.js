cd327fde4fb870de815c66f47ef4d31bbf344230
anestan <anes.tan@gmail.com>
2022-05-24 11:59:49 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
